011037e0d963c0b71e1a8fb886262e57ab15af5b
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-06-20 21:11:57 +0530

    Ansible

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {}
+variable "subnets" {}
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_alb_cidr" {}
+variable "tags" {}
+variable "internal" {}
